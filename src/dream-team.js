3cfd089c2a989a7744b304c668f4044124985d54
r144yh <mrakur@yandex.ru>
2020-10-27 01:43:21 +0300

    feat: add three js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let dreamTeam = '';
+    if (members === null || members === undefined) return false
+    for (let i = 0; i < members.length; i++) {
+        if (typeof members[i] === 'string') {
+            dreamTeam += members[i].trim().charAt(0)
+        }
+    }
+    dreamTeam = dreamTeam.toUpperCase();
+    return dreamTeam.split('').sort().join('');
 };
