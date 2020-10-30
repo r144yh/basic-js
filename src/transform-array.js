3e062c8770f9d056a93da6a635ed573b5afed790
r144yh <mrakur@yandex.ru>
2020-10-30 22:13:19 +0300

    feat: so close end task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,43 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
 
+    if (typeof arr !== 'object') throw "Not an array";
+    if (arr.length === 0) return [];
+    function checkIndex(arr, newArr, index){
+        return arr[index] !== undefined && newArr[index] !== undefined;
+    }
+    // let newArr = arr;
+    let newArr = arr;
+    for (let index = 0; index < arr.length; index++) {
+        switch (arr[index]) {
+            case '--discard-next':
+                delete newArr[index];
+                if (!checkIndex(arr, newArr, index + 1)) break;
+
+                delete newArr[index+1];
+                break;
+            case '--discard-prev':
+                delete newArr[index];
+                if (!checkIndex(arr, newArr, index - 1)) break;
+
+                delete newArr[index-1];
+                break;
+            case '--double-next':
+                delete newArr[index];
+                if (!checkIndex(arr, newArr, index + 1)) break;
+                newArr[index] = newArr[index+1];
+                index++;
+                break;
+            case '--double-prev':
+                delete newArr[index];
+                if (!checkIndex(arr, newArr, index - 1)) break;
+                newArr[index] = newArr[index-1];
+                break;
+        }
+    }
+    for (let index = newArr.length - 1; index >= 0; --index){
+        if (newArr[index] === undefined) {
+            newArr.splice(index, 1);
+        }
+    }
+    return newArr
 };
