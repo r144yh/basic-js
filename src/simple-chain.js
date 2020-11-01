234bd0fe8423846b400d00ea017e20506c30f267
r144yh <mrakur@yandex.ru>
2020-11-01 14:41:31 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,20 +1,39 @@
 const chainMaker = {
+    strArr: [],
 
-  getLength() {
-    return this;
-  },
-  addLink(value) {
-    return this;
-  },
-  removeLink(position) {
-    return this;
-  },
-  reverseChain() {
-    return this;
-  },
-  finishChain() {
-    return this;
-  }
+    getLength() {
+        return this.strArr.length;
+    },
+
+    addLink(value) {
+        if (value !== null || true) {
+            this.strArr.push(`( ${value} )`);
+        } else {
+            this.strArr.push(`( )`);
+        }
+        return this
+    },
+
+    removeLink(position) {
+        if (typeof position !== 'number' || this.strArr[position] === undefined || !Number.isInteger(position)) {
+            this.strArr = [];
+            throw 'Error'
+        } else {
+            this.strArr.splice(position - 1, 1);
+        }
+        return this
+    },
+
+    reverseChain() {
+        this.strArr.reverse();
+        return this
+    },
+
+    finishChain() {
+        let str = this.strArr.join('~~');
+        this.strArr = [];
+        return str
+    }
 };
 
 module.exports = chainMaker;
